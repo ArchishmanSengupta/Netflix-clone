92f21f70da40e0595aee950ccf9e02ec2ddde41f
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-03-26 23:01:47 +0530

    navbar skeleton made

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBbumCsyhGL5OR80L8qXM5FOlmT4DCmP_0",
+    authDomain: "netflix-clone-6c394.firebaseapp.com",
+    projectId: "netflix-clone-6c394",
+    storageBucket: "netflix-clone-6c394.appspot.com",
+    messagingSenderId: "1031537318800",
+    appId: "1:1031537318800:web:08a7dbdbf3c48a1055f115"
+};
---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Nav.css"
+function Nav() {
+    return (
+        <div className="nav nav__black">
+            <div className="nav__contents">
+            <img 
+            className="nav__logo"
+            src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
+            alt=""/>
+            <img
+            className="nav__avatar" 
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png" 
+            alt=""/>
+            </div>
+        </div>
+    )
+}
+
+export default Nav

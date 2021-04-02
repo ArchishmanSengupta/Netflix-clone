f769fd26713aac811e8b3ecfb0166f5a41071b27
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-04-03 00:19:03 +0530

    header part done

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,47 +1,14 @@
 import React from 'react'
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import Banner from "./Banner.js"
 
 function HomeScreen() {
     return (
         <div className="homeScreen">
             <Nav />
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
+        <Banner />
 
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
         </div>
     )
 }

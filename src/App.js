eb42579111c72834f4d75498db145359ec1719a1
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-06-18 19:54:32 +0530

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import HomeScreen from "./HomeScreen";
+// React router Dom
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-     <HomeScreen />
+     
+
+     <Router>
+        <Switch>
+          <Route path="/">
+          <HomeScreen />
+          </Route>
+        </Switch>
+      
+    </Router>
     </div>
   );
 }

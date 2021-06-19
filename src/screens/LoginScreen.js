0f59638f575d7e4be8a2beedec0afdafc5fc88d6
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-06-19 23:18:31 +0530

    added headings

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -7,6 +7,13 @@ function LoginScreen(){
             <img className="loginScreen__logo"src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
             <button className="loginScreen__button"> Sign In</button>
             <div className="loginScreen__gradient"></div>
+            <div className="loginScreen__body">
+                <>
+                <h1>Ulimited films, TV programmes and more.</h1>
+                <h2> Watch anywhere. Cancel at any time</h2>
+                <h3> Ready to watch? Enter your email to create or restart your membership</h3>
+                </>
+            </div>
         </div>
 
     </div>

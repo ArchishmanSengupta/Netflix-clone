5e5202f89f8319d4c7f77e9437705f6300688345
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-08-02 19:35:29 +0530

    firebase auth

--- src/screens/firebase.js
+++ src/screens/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBbumCsyhGL5OR80L8qXM5FOlmT4DCmP_0",
     authDomain: "netflix-clone-6c394.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "1031537318800",
     appId: "1:1031537318800:web:08a7dbdbf3c48a1055f115"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export {auth};
+export default db;
